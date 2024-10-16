0fc0a9cfdf3087afe86108671720a7fea1cbb776
Emanuel Gallo <emanuelgallodg@gmail.com>
2024-10-16 15:31:46 -0300

    Update Structure Class Role

--- eximius-back/src/main/java/com/eximius/eximius/Entities/Role.java
+++ eximius-back/src/main/java/com/eximius/eximius/Entities/Role.java
@@ -1,4 +1,20 @@
 package com.eximius.eximius.Entities;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "roles")
 public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_role")
+    private Long idRole;
+    private String name;
 }
